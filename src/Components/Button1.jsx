2e2a3e155e5e8f64e77d70be5dd562fc11c9dcad
olumide-as <adigunsamuel84@gmail.com>
2024-09-23 22:37:19 +0100

    push

--- src/Components/Button1.jsx
+++ src/Components/Button1.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button1 = ({ image, text }) => {
   return (
     <div>
-      <button className="w-full bg-white rounded-2xl flex items-center justify-center p-2">
+      <button className="w-full bg-white rounded-2xl flex flex-col  justify-center p-2">
         <img src={image} alt="button image" className="w-6 mr-2" />
         <p className="text-xs">{text}</p>
       </button>
